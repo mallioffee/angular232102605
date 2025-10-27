161b7ad4f907d54252f2b3200953804404c0c62a
mallioffee <mallioffee@gmail.com>
2025-10-27 10:28:59 +0700

    Update

--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { Header } from "../header/header";
+import { Sidebar } from "../sidebar/sidebar";
+import { Footer } from "../footer/footer";
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [RouterModule, Header, Sidebar, Footer],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
 })
