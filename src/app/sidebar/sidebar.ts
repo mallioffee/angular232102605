e45f1ae87c0ac8a74cd673f21c3aaab386f0e76b
mallioffee <mallioffee@gmail.com>
2025-10-30 17:19:23 +0700

    update

--- src/app/sidebar/sidebar.ts
+++ src/app/sidebar/sidebar.ts
@@ -1,12 +1,28 @@
-import { Component, Input, input } from '@angular/core';
+import { Component, Input, ElementRef, Renderer2, AfterViewInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [ RouterModule],
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './sidebar.html',
-  styleUrl: './sidebar.css'
+  styleUrls: ['./sidebar.css']
 })
-export class Sidebar {
- @Input() modulName: string = "";
+export class Sidebar implements AfterViewInit {
+  @Input() modulName: string = "";
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  ngAfterViewInit(): void {
+    const links = this.el.nativeElement.querySelectorAll('.nav-link');
+    links.forEach((link: HTMLElement) => {
+      this.renderer.listen(link, 'click', () => {
+        const body = document.querySelector('body');
+        if (body?.classList.contains('sidebar-open')) {
+          this.renderer.removeClass(body, 'sidebar-open');
+          this.renderer.addClass(body, 'sidebar-collapse');
+        }
+      });
+    });
+  }
 }
