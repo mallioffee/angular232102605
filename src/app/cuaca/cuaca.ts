af24a913806e4955d11dd9f1c57000fbce4697b7
mallioffee <mallioffee@gmail.com>
2025-12-15 10:58:46 +0700

    Update cuaca.ts

--- src/app/cuaca/cuaca.ts
+++ src/app/cuaca/cuaca.ts
@@ -1,7 +1,7 @@
 import { AfterViewInit, Component, Renderer2 } from '@angular/core';
-import { Header } from "../header/header";
-import { Sidebar } from "../sidebar/sidebar";
-import { RouterLink, RouterLinkActive } from "@angular/router";
+import { Header } from '../header/header';
+import { Sidebar } from '../sidebar/sidebar';
+import { RouterLink, RouterLinkActive, RouterModule } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 
 declare const $: any;
@@ -9,41 +9,38 @@ declare const moment: any;
 
 @Component({
   selector: 'app-cuaca',
-  imports: [Header, Sidebar, RouterLink, RouterLinkActive],
+  imports: [Header, Sidebar, RouterModule, RouterLink, RouterLinkActive],
   templateUrl: './cuaca.html',
   styleUrl: './cuaca.css',
 })
 export class Cuaca implements AfterViewInit {
   private table1: any;
 
-  constructor(private renderer: Renderer2, private http: HttpClient) {
+  constructor(private renderer:Renderer2, private http: HttpClient) {
     this.renderer.removeClass(document.body, "sidebar-open");
     this.renderer.addClass(document.body, "sidebar-closed");
   }
 
   ngAfterViewInit(): void {
-    this.table1 = $('#table1').DataTable({
+    this.table1 = $("#table1").DataTable({
       columnDefs: [
         {
           targets: 0,
-          render: function (data: string) {
-            const waktu = moment(data + "UTC");
+          render: function (data:string) {
+            const waktu = moment(data + " UTC");
             console.log(waktu);
 
             const html =
-              waktu.local().format("YYYY-MM-DD") + "<br/>" + waktu.local().format("HH:mm") + " WIB";
+            waktu.local().format("YYYY-MM-DD") + "<br />" + waktu.local().format("HH:mm") + " WIB";
 
             return html;
-          }, 
-        },  
-        {
+          },
+        }, {
           targets: [1],
           render: function (data: string) {
-
             return "<img src='" + data + "' style='filter: drop-shadow(5px 5px 10px rgba(0, 0, 0, 0.7));' />";
           }
-        }, 
-        {
+        }, {
           targets: [2],
           render: function (data: string) {
             const array = data.split("||");
@@ -55,15 +52,17 @@ export class Cuaca implements AfterViewInit {
           },
         },
       ],
-
     });
   }
 
-  getData(city: string): void {
+  getData(city:string): void {
     city = encodeURIComponent(city);
 
-    this.http.get('http://api.openweathermap.org/data/2.5/forecast?q=pontianak&appid=a10addd193b7887966a21c529dcbfc47')
-    .subscribe((data: any) => {
+    this.http
+   .get(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=a10addd193b7887966a21c529dcbfc47`)
+
+
+    .subscribe((data:any) => {
       let list = data.list;
       console.log(list);
 
@@ -73,34 +72,35 @@ export class Cuaca implements AfterViewInit {
         const weather = element.weather[0];
         console.log(weather);
 
-        const iconUrl = "http://openweathermap.org/img/wn/" + weather.icon + "@2x.png";
-        const cuacaDeskripsi = weather.main + "|| " + weather.description;
+        const iconUrl = "https://openweathermap.org/img/wn/" + weather.icon + "@2x.png";
+        const cuacaDeskripsi = weather.main + "||" + weather.description;
 
         const main = element.main;
         console.log(main);
 
         const tempMin = this.kelvinToCelcius(main.temp_min);
-        console.log({ tempMin });
+        console.log({ tempMin});
 
         const tempMax = this.kelvinToCelcius(main.temp_max);
-        console.log({ tempMax });
+        console.log({ tempMax});
 
-        const temp = tempMin + "°C / " + tempMax + "°C";
+        const temp = tempMin + "°C - " + tempMax + "°C";
 
-        const row =  [element.dt_txt, iconUrl, cuacaDeskripsi, temp];
+        const row = [element.dt_txt, iconUrl, cuacaDeskripsi, temp];
 
         this.table1.row.add(row);
       });
 
       this.table1.draw(false);
-    },  (error: any) => {
-        alert(error.error.message);
-        this.table1.clear();
-        this.table1.draw(false);
+    }, (error:any) => {
+      alert(error.error.message);
+      this.table1.clear();
+      this.table1.draw(false);
     });
-  }
 
-  kelvinToCelcius(kelvin: any): any {
+    }
+
+  kelvinToCelcius(kelvin: any): any{
     let celcius = kelvin - 273.15;
     celcius = Math.round(celcius * 100) / 100;
 
@@ -110,12 +110,12 @@ export class Cuaca implements AfterViewInit {
   handleEnter(event: any) {
     const cityName = event.target.value;
 
-    if (cityName =="") {
+    if (cityName == "") {
       this.table1.clear();
-      this.table1.draw();
+      this.table1.draw(false);
     }
 
     this.getData(cityName);
-  }
+  }
 
 }
