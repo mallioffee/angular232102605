90ad6360dc9e0324e24b48f213020c61bd2f746e
mallioffee <mallioffee@gmail.com>
2026-01-06 05:51:33 +0700

    update

--- src/app/sidebar/sidebar.ts
+++ src/app/sidebar/sidebar.ts
@@ -1,17 +1,22 @@
 import { Component, Input, ElementRef, Renderer2, AfterViewInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
   imports: [RouterModule],
   templateUrl: './sidebar.html',
-  styleUrls: ['./sidebar.css']
+  styleUrls: ['./sidebar.css'],
 })
 export class Sidebar implements AfterViewInit {
-  @Input() modulName: string = "";
+  @Input() modulName: string = '';
 
-  constructor(private el: ElementRef, private renderer: Renderer2) {}
+  constructor(
+    private el: ElementRef,
+    private renderer: Renderer2,
+    private cookieService: CookieService
+  ) {}
 
   ngAfterViewInit(): void {
     const links = this.el.nativeElement.querySelectorAll('.nav-link');
@@ -24,6 +29,49 @@ export class Sidebar implements AfterViewInit {
         }
       });
     });
+
+    const saved = localStorage.getItem('adminlte-theme');
+    const body = document.body;
+    const header = document.querySelector('.main-header') as HTMLElement;
+
+    if (saved === 'dark') {
+      body.classList.add('dark-mode');
+
+      if (header) {
+        header.classList.remove('navbar-white', 'navbar-light');
+        header.classList.add('navbar-dark', 'navbar-primary');
+      }
+    } else {
+      if (header) {
+        header.classList.remove('navbar-dark', 'navbar-primary');
+        header.classList.add('navbar-white', 'navbar-light');
+      }
+    }
   }
-  
+
+  toggleTheme() {
+    const body = document.body;
+    const header = document.querySelector('.main-header') as HTMLElement;
+
+    const isDark = body.classList.contains('dark-mode');
+
+    // toggle body
+    body.classList.toggle('dark-mode');
+
+    // toggle header class AdminLTE
+    if (header) {
+      if (!isDark) {
+        // dari light → dark
+        header.classList.remove('navbar-white', 'navbar-light');
+        header.classList.add('navbar-dark', 'navbar-primary'); // atau navbar-dark navbar-dark
+      } else {
+        // dari dark → light
+        header.classList.remove('navbar-dark', 'navbar-primary');
+        header.classList.add('navbar-white', 'navbar-light');
+      }
+    }
+
+    // simpan tema
+    localStorage.setItem('adminlte-theme', isDark ? 'light' : 'dark');
+  }
 }
